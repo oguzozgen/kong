01f44d8fe0e0bb8e71836f986a858036c861ab1f
Enrique García Cota <kikito@gmail.com>
2018-09-16 11:55:00 +0200

    feat(schema) `contains` validator

--- kong/db/schema/metaschema.lua
+++ kong/db/schema/metaschema.lua
@@ -46,6 +46,7 @@ local validators = {
   { match_any = match_any_list },
   { starts_with = { type = "string" }, },
   { one_of = { type = "array", elements = { type = "string" } }, },
+  { contains = { type = "any" }, },
   { is_regex = { type = "boolean" }, },
   { timestamp = { type = "boolean" }, },
   { uuid = { type = "boolean" }, },
@@ -178,6 +179,10 @@ local attribute_types = {
     ["string"] = true,
     ["number"] = true,
     ["integer"] = true,
+  },
+  contains = {
+    ["array"] = true,
+    ["set"]   = true,
   },
   is_regex = {
     ["string"] = true,
